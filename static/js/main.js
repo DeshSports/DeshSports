0d7e8d72a750538fbaf25cc26ecedeaf519c62c5
SakibAbrar <sakib.cghs@gmail.com>
2020-12-18 02:28:16 +0600

    Added fonts and finalized pc view

--- static/js/main.js
+++ static/js/main.js
@@ -130,6 +130,41 @@
     });
 
 
+    // Category page slider
+    $('.cp-slider').slick({
+        autoplay: false,
+        infinite: true,
+        dots: false,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        responsive: [{
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 2
+                }
+            }
+        ]
+    });
+
+
     // Back to top button
     $(window).scroll(function() {
         if ($(this).scrollTop() > 100) {
