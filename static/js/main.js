fa23b0756c27e14885249eecc8c62a0a6107154a
SakibAbrar <sakib.cghs@gmail.com>
2020-12-29 14:11:08 +0600

    changed on category page

--- static/js/main.js
+++ static/js/main.js
@@ -152,13 +152,13 @@
             {
                 breakpoint: 768,
                 settings: {
-                    slidesToShow: 2
+                    slidesToShow: 1
                 }
             },
             {
                 breakpoint: 576,
                 settings: {
-                    slidesToShow: 2
+                    slidesToShow: 1
                 }
             }
         ]
